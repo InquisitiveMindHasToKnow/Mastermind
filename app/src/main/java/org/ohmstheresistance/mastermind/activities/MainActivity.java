84e323f05e8907a213807f47563a9887691d60ed
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2020-01-22 11:32:22 -0500

    Successful response using OKHttp to get random numbers.

--- app/src/main/java/org/ohmstheresistance/mastermind/activities/MainActivity.java
+++ app/src/main/java/org/ohmstheresistance/mastermind/activities/MainActivity.java
@@ -12,7 +12,14 @@ import android.widget.Toast;
 
 import org.ohmstheresistance.mastermind.R;
 
+import java.io.IOException;
 import java.util.Locale;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -22,6 +29,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView previousGuessesHeaderTextView;
     private TextView guessesRemainingTextView;
     private TextView countDownTimerTextView;
+    private TextView randomNumbersTextView;
+    private TextView randomNumbersTextViewTwo;
+
     private Button zeroButton;
     private Button oneButton;
     private Button twoButton;
@@ -38,6 +48,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private int totalGuesses = 10;
     private long timeLeftInMillis;
 
+    private String randomNumbersResponse;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +59,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         timeLeftInMillis = COUNTDOWN_TIMER_IN_MILLIS;
 
         setUpViews();
+        getRandomNumbers();
         startCountDown();
     }
 
@@ -56,6 +69,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         previousGuessesHeaderTextView = findViewById(R.id.previous_guesses_header_textview);
         guessesRemainingTextView = findViewById(R.id.remaining_guess_count_textview);
         countDownTimerTextView = findViewById(R.id.countdown_timer_textview);
+        randomNumbersTextView = findViewById(R.id.random_numbers_textview);
+        randomNumbersTextViewTwo = findViewById(R.id.random_numbers_textview_two);
 
         zeroButton = findViewById(R.id.zero_button);
         oneButton = findViewById(R.id.one_button);
@@ -79,6 +94,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         sevenButton.setOnClickListener(this);
         deleteButton.setOnClickListener(this);
         guessButton.setOnClickListener(this);
+
     }
 
     @Override
@@ -135,8 +151,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             case R.id.guess_button:
 
+
                 if (totalGuesses > 0) {
+
+                    if(userGuessEditText.getText().toString().length() < 4 && userGuessEditText.getText().toString().length() >= 1 ){
+                        Toast.makeText(this, "Please enter a 4 digit combination.", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    if(userGuessEditText.getText().toString().isEmpty()){
+                        Toast.makeText(this, "Please enter a valid entry.", Toast.LENGTH_SHORT).show();
+
+                        return;
+                    }
+
                     totalGuesses--;
+                    guessesRemainingTextView.setText(totalGuesses + "");
+
                 } else {
 
                     guessButton.setEnabled(false);
@@ -144,6 +174,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 break;
         }
+    }
+
+    private void getRandomNumbers(){
+        OkHttpClient client = new OkHttpClient();
+
+        String url = "https://www.random.org/integers/?num=4&min=0&max=7&col=4&base=10&format=plain&rnd=new";
+
+        String baseOfNumers = "10";
+        String col = "1";
+        String num = "4";
+        String minNum = "0";
+        String maxNum = "7";
+        String format = "plain";
+        String rnd = "new";
+
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    randomNumbersResponse = response.body().string();
+
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            randomNumbersTextView.setText(randomNumbersResponse);
+                            randomNumbersTextViewTwo.setText(randomNumbersResponse);
+                        }
+                    });
+                }
+            }
+        });
     }
 
     private void startCountDown() {
